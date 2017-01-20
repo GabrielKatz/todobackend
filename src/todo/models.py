61fab93fc418fc6d441e6fdd6f1f6aed5a4f41a2
Gabriel Katz <gkatz@gmx.net>
2017-01-20 11:14:13 +0100

    got migration to work

--- src/todo/models.py
+++ src/todo/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class TodoItem(models.Model)
+class TodoItem(models.Model):
 	title = models.CharField(max_length=256, null=True, blank=True)
 	completed = models.BooleanField(blank=True, default=False)
 	url = models.CharField(max_length=256, null=True, blank=True)
